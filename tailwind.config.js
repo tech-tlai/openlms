c49a5eddea59e26a3615244199a574e787fe266f
thalmis-zt <thalmis.faraj@zaeemsolutions.com>
2025-01-16 13:07:46 +0530

    final corrections in ui

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        lightTeal: "#E5F6F4",
+        teal: "#40B3A2",
+        orange: "#FF5722",
+        orange500: "#f97316",
+        lightOrange: "#FFEDE5",
+        darkGray: "#4A4A4A",
+        lightGray: "#EAEAEA",
+        darkBlue: "#1B365D",
+      },
+    },
+  },
+  plugins: [],
+};
